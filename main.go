6a77bb2b97fae8bf254e954f04aa7edb47be40a5
JustNZ <51332147+JustNZ@users.noreply.github.com>
2025-03-22 11:48:48 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,160 @@
+package main
+
+import (
+	"errors"
+	"net/rpc"
+	"time"
+
+	"github.com/v1Flows/runner/pkg/executions"
+	"github.com/v1Flows/runner/pkg/plugins"
+
+	"github.com/v1Flows/shared-library/pkg/models"
+
+	"github.com/hashicorp/go-plugin"
+)
+
+// Plugin is an implementation of the Plugin interface
+type Plugin struct{}
+
+func (p *Plugin) ExecuteTask(request plugins.ExecuteTaskRequest) (plugins.Response, error) {
+	param1 := ""
+
+	// access action params
+	for _, param := range request.Step.Action.Params {
+		if param.Key == "Param1" {
+			param1 = param.Value
+		}
+	}
+
+	// update the step with the messages
+	err := executions.UpdateStep(request.Config, request.Execution.ID.String(), models.ExecutionSteps{
+		ID: request.Step.ID,
+		Messages: []models.Message{
+			{
+				Title: "Templated",
+				Lines: []string{
+					"Starting template action",
+				},
+			},
+		},
+		Status:    "running",
+		StartedAt: time.Now(),
+	}, request.Platform)
+	if err != nil {
+		return plugins.Response{
+			Success: false,
+		}, err
+	}
+
+	// update the step with the messages
+	err = executions.UpdateStep(request.Config, request.Execution.ID.String(), models.ExecutionSteps{
+		ID: request.Step.ID,
+		Messages: []models.Message{
+			{
+				Title: "Templated",
+				Lines: []string{
+					"Execution ID: " + request.Execution.ID.String(),
+					"Step ID: " + request.Step.ID.String(),
+					param1,
+					"Template Action finished",
+				},
+			},
+		},
+		Status:     "success",
+		StartedAt:  time.Now(),
+		FinishedAt: time.Now(),
+	}, request.Platform)
+	if err != nil {
+		return plugins.Response{
+			Success: false,
+		}, err
+	}
+
+	return plugins.Response{
+		Success: true,
+	}, nil
+}
+
+func (p *Plugin) EndpointRequest(request plugins.EndpointRequest) (plugins.Response, error) {
+	return plugins.Response{
+		Success: false,
+	}, errors.New("not implemented")
+}
+
+func (p *Plugin) Info(request plugins.InfoRequest) (models.Plugin, error) {
+	var plugin = models.Plugin{
+		Name:    "Template",
+		Type:    "action",
+		Version: "1.0.0",
+		Author:  "JustNZ",
+		Action: models.Action{
+			Name:        "Template",
+			Description: "Template description",
+			Plugin:      "template",
+			Icon:        "solar:clipboard-list-broken",
+			Category:    "Template",
+			Params: []models.Params{
+				{
+					Key:         "Param1",
+					Type:        "text",
+					Default:     "",
+					Required:    false,
+					Description: "Param1 description",
+				},
+			},
+		},
+		Endpoint: models.Endpoint{},
+	}
+
+	return plugin, nil
+}
+
+// PluginRPCServer is the RPC server for Plugin
+type PluginRPCServer struct {
+	Impl plugins.Plugin
+}
+
+func (s *PluginRPCServer) ExecuteTask(request plugins.ExecuteTaskRequest, resp *plugins.Response) error {
+	result, err := s.Impl.ExecuteTask(request)
+	*resp = result
+	return err
+}
+
+func (s *PluginRPCServer) EndpointRequest(request plugins.EndpointRequest, resp *plugins.Response) error {
+	result, err := s.Impl.EndpointRequest(request)
+	*resp = result
+	return err
+}
+
+func (s *PluginRPCServer) Info(request plugins.InfoRequest, resp *models.Plugin) error {
+	result, err := s.Impl.Info(request)
+	*resp = result
+	return err
+}
+
+// PluginServer is the implementation of plugin.Plugin interface
+type PluginServer struct {
+	Impl plugins.Plugin
+}
+
+func (p *PluginServer) Server(*plugin.MuxBroker) (interface{}, error) {
+	return &PluginRPCServer{Impl: p.Impl}, nil
+}
+
+func (p *PluginServer) Client(b *plugin.MuxBroker, c *rpc.Client) (interface{}, error) {
+	return &plugins.PluginRPC{Client: c}, nil
+}
+
+func main() {
+	plugin.Serve(&plugin.ServeConfig{
+		HandshakeConfig: plugin.HandshakeConfig{
+			ProtocolVersion:  1,
+			MagicCookieKey:   "PLUGIN_MAGIC_COOKIE",
+			MagicCookieValue: "hello",
+		},
+		Plugins: map[string]plugin.Plugin{
+			"plugin": &PluginServer{Impl: &Plugin{}},
+		},
+		GRPCServer: plugin.DefaultGRPCServer,
+	})
+}
